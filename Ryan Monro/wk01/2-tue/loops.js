c787c086d13580c2a0567f1a1453f1ad57108225
ryanmonro <ryan@ryanmonro.info>
2017-08-29 15:50:51 +1000

    Fixed money tree, multiplication bonus task

--- Ryan Monro/wk01/2-tue/loops.js
+++ Ryan Monro/wk01/2-tue/loops.js
@@ -1,4 +1,5 @@
 // Even/Odd Reporter
+console.log("Even/Odd Reporter");
 for (var count = 0; count <= 20; count++){
   if (count % 2 === 0) {
     console.log(count + " is even");
@@ -8,12 +9,20 @@ for (var count = 0; count <= 20; count++){
 }
 
 // Multiplication Tables
+console.log("\nMultiplication");
 for (var multiplier = 0; multiplier <= 10; multiplier++){
   console.log(multiplier + " * 9 = " + multiplier * 9);
 }
+// Bonus: nested loop for tables from 1 to 10
+console.log("\nMultiplication Tables from 1 to 10:")
+for (var secondNumber = 1; secondNumber <= 10; secondNumber++){
+  for (var firstNumber = 1; firstNumber <= 10; firstNumber++){
+    console.log(firstNumber + " * " + secondNumber + " = " + secondNumber * firstNumber);
+  }
+}
 
 // Top Choices
-console.log("My Top TV Shows:");
+console.log("\nMy Top TV Shows:");
 var myTopTVShows = ["Better Call Saul", 
   "Rick and Morty", 
   "Twin Peaks", 
@@ -24,31 +33,33 @@ var myTopTVShows = ["Better Call Saul",
   "Futurama", 
   "The Simpsons",
   "Breaking Bad"];
-for (var iteration = 0; iteration < myTopTVShows.length; iteration++){
+for (var index = 0; index < myTopTVShows.length; index++){
   var suffix = "th";
-  var index = iteration + 1;
-  if (index === 1) {
+  var number = index + 1;
+  // replace suffix if number is 1, 2 or 3
+  if (number === 1) {
     suffix = "st";
-  } else if (index === 2){
+  } else if (number === 2){
     suffix = "nd";
-  } else if (index === 3){
+  } else if (number === 3){
     suffix = "rd";
   }
-  console.log("My " + index + suffix + " choice: " + myTopTVShows[index]);
+  console.log("My " + number + suffix + " choice: " + myTopTVShows[index]);
 }
 
-// Pattern
+// The Money Tree!
+console.log("\n💰The Money Tree💰");
 // There are five rows
-for (var row = 0; row < 5; row ++){
-  // start with empty row
+for (var row = 0; row < 5; row++){
+  // start with an empty row
   var rowPattern = "";
-  // add spaces at beginning, beginning with 0 for 0th row and increasing
-  for (var spaceCount = 0; spaceCount < row; spaceCount++){
+  // add spaces to row, beginning with 4 for 0th row and decreasing
+  for (var spaceCount = 4; spaceCount > row; spaceCount--){
     rowPattern += " ";
   }
-  // add a $ followed by a space beginning with 5 for row 0, decreasing 
-  for (var count = 5; count > row; count--){
-    rowPattern += "$ ";
+  // add a $ followed by a space beginning with 1 for row 0, increasing by 2 per row 
+  for (var count = 0; count <= row * 2; count++){
+    rowPattern += "$";
   }
   // output row to console
   console.log(rowPattern);
